de211c1b55a31e1e7caddb0cff88849088f9c616
Deep-sea-lab <3800907852@qq.com>
2025-08-20 13:36:10 +0800

    Adding files

---
+++ src/lib/libraries/tw-extension-tags.js
@@ -0,0 +1,9 @@
+import {APP_NAME} from '../brand';
+
+// Because there are all brand names, it is unnecessary for them to be translatable.
+export default [
+    {tag: 'scratch', intlLabel: 'Scratch'},
+    {tag: 'tw', intlLabel: APP_NAME},
+    {tag: 'ztengine', intlLabel: '02Engine'},
+    {tag: 'pm', intlLabel: 'PenguinMod'}
+];
